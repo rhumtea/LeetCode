c24d9de2afb03410ac441f863b8c479f33a801fb
RhumTea <160001872+rhumtea@users.noreply.github.com>
2024-04-29 23:35:18 -0700

    Time: 271 ms (15.83%), Space: 16.9 MB (8.49%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -1,20 +1,13 @@
 class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
-        worktime = {}
-        worktime[logs[0][0]] = logs[0][1]
+        maxTime = logs[0][1]
+        smallID = logs[0][0]
         for i in range(1, len(logs)):
             temp = logs[i][1] - logs[i-1][1]
-            if logs[i][0] not in worktime:
-                worktime[logs[i][0]] = temp
-            else:
-                worktime[logs[i][0]] = max(worktime[logs[i][0]], temp)
-        key = list(worktime.keys())
-        value = list(worktime.values())
-        maxTime, smallID = 0, n
-        for i in range(len(value)):
-            if value[i] > maxTime:
-                maxTime = value[i]
-                smallID = key[i]
-            elif value[i] == maxTime and key[i] < smallID:
-                smallID = key[i]
+            print(temp)
+            if maxTime < temp:
+                maxTime = temp
+                smallID = logs[i][0]
+            elif maxTime == temp:
+                smallID = min(smallID, logs[i][0])
         return smallID
