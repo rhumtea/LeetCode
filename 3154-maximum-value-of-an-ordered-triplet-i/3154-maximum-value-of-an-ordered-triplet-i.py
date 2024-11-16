731f3e79b67203da96468c83c4146fdad62bf58f
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2024-11-15 19:40:27 -0800

    Time: 0 ms (100%), Space: 16.5 MB (70.63%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        res = diff = prefix = 0 
+        res = diff = maxNum = 0 
         for i in nums: 
             res = max(res, i * diff)
-            diff = max(diff, prefix - i)
-            prefix = max(prefix, i)
+            diff = max(diff, maxNum - i) # keep track max diff
+            maxNum = max(maxNum, i) # store max number from start to current
         return res
                 
