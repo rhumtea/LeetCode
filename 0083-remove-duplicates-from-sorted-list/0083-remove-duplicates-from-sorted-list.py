893bb18fbe7a8c02e69cbf1d80ab2595c01a8fd7
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-19 12:11:28 -0700

    Time: 0 ms (100%), Space: 17.7 MB (90.72%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -8,12 +8,12 @@ class Solution:
         if head == None or head.next == None:
             return head
         c = head
-        while c.next.next:
-            if c.val == c.next.val:
-                t = c.next.next
+        t = c.next
+        while t:
+            if c.val == t.val:
+                t = t.next
                 c.next = t
             else:
                 c = c.next
-        if c.val == c.next.val:
-            c.next = None
+                t = t.next
         return head
