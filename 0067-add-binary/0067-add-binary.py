70c7a461f21b4f8db3c056c62c11fbc45983fee2
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-19 10:46:45 -0700

    Time: 3 ms (46.15%), Space: 17.8 MB (73.42%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,23 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        cout = i = j = 0
+        a = a[::-1]
+        b = b[::-1]
+        while i < len(a) or j < len(b) or cout:
+            temp = 0
+            temp += cout
+            if i < len(a):
+                temp += int(a[i])
+                i += 1
+            if j < len(b):
+                temp += int(b[j])
+                j += 1
+            res.append(str(temp % 2))
+            cout = temp // 2
+        return ''.join(res[::-1])
+
+
+
+        
+                
