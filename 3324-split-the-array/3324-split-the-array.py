a0431c2942b17488b7c22ef7b46bc5f28d73f94c
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-25 07:32:04 -0700

    Time: 1 ms (52.08%), Space: 17.7 MB (67.47%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.py
+++ 3324-split-the-array/3324-split-the-array.py
@@ -4,12 +4,7 @@ class Solution:
         nums1 = nums2 = 0
         for num in nums:
             mp[num] += 1
-        for k,v in mp.items():
+        for v in mp.values():
             if v > 2:
                 return False
-            elif v == 1:
-                if nums1 < nums2:
-                    nums1 += 1
-                else:
-                    nums2 += 1
-        return nums1 == nums2
+        return True
