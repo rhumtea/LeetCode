6e3845afe0b73964facc8a9766f68baa1b9ed3f1
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-25 07:34:31 -0700

    Time: 78 ms (87.1%), Space: 27.8 MB (43.03%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -9,12 +9,6 @@ class Solution:
         a = headA
         b = headB
         while a != b:
-            if a:
-                a = a.next
-            else:
-                a = headB
-            if b:
-                b = b.next
-            else: 
-                b = headA
+            a = a.next if a else headB
+            b = b.next if b else headA
         return a
