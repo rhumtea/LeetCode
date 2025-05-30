1e68d20b36ae44c35260c9ab3ed42988fcce3343
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-30 13:56:02 -0700

    Time: 47 ms (97.18%), Space: 23 MB (83.63%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,16 +1,17 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def helper(nums, k):
-            res = l = w = 0
-            for r in range(len(nums)):
-                if nums[r]%2: w += 1
-                while w > k:
-                    if nums[l]%2: w -= 1
+        res = 0
+        w = 0
+        l = gap = 0
+        for r in range(len(nums)):
+            if nums[r]%2:
+                w += 1
+            if w == k:
+                gap = 0
+                while w == k:
+                    if nums[l]%2:
+                        w -= 1
+                    gap += 1
                     l += 1
-                res += r-l+1
-            return res
-        a = helper(nums, k)
-        b = helper(nums, k-1)
-        return a-b
-
-        
+            res += gap
+        return res                
