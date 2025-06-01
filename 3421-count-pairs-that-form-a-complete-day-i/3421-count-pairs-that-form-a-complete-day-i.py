093820f47bc590148267e740322ec383aa1caca1
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-31 22:38:05 -0700

    Time: 8 ms (80%), Space: 18.1 MB (11.38%) - LeetHub

--- 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.py
+++ 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.py
@@ -4,9 +4,7 @@ class Solution:
         mp = defaultdict(int)
         for hour in hours:
             print("hour", hour%24)
-            if (24 - hour%24) in mp:
-                res += mp[24 - hour%24]
-            if hour%24 == 0 and hour%24 in mp:
-                res += mp[0]
+            if (24 - hour%24)%24 in mp:
+                res += mp[(24 - hour%24)%24]
             mp[hour%24] += 1
         return res
