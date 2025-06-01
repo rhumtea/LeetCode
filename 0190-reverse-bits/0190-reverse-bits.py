0f44380574c4f26e91fb8a251f72eec03d11ba46
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-05-31 23:18:03 -0700

    Time: 47 ms (10.99%), Space: 17.8 MB (36.5%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        for i in range(32):
+        arr = [0] * 32
+        i = 0
+        while n > 0:
             d = n & 1
-            res = (res << 1) | d
-            n = n >> 1
+            arr[i] = d
+            n >>= 1
+            i += 1
+        for i in range(len(arr)):
+            res += arr[::-1][i] * 1 << i
         return res
