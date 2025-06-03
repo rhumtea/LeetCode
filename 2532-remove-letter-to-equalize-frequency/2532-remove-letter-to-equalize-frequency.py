4770b9b79f8d6223e429adc16f750d0fa2e0891a
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-06-03 11:49:43 -0700

    Time: 0 ms (100%), Space: 17.6 MB (98.07%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -3,13 +3,11 @@ class Solution:
         mp = defaultdict(int)
         for c in word:
             mp[c] += 1
-        mp1 = defaultdict(int)
-        for v in mp.values():
-            mp1[v] += 1
-        if len(mp1) == 1:
-            return list(mp1.keys())[0] == 1 or list(mp1.values())[0] == 1
-        if len(mp1) == 2:
-            max_key = max(mp1.keys())
-            min_key = min(mp1.keys())
-            return ((max_key - min_key) == mp1[max_key] == 1) or (min_key == mp1[min_key] == 1)
+        for c in word:
+            mp[c] -= 1
+            if mp[c] == 0:
+                mp.pop(c)
+            if len(set(mp.values())) == 1:
+                return True
+            mp[c] += 1
         return False
