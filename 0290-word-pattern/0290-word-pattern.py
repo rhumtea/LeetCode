f6d70888b1b0c4d8a7c085cfa6e4d1030675f6d0
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-06-08 22:53:44 -0700

    Time: 0 ms (100%), Space: 17.7 MB (86.84%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -6,12 +6,10 @@ class Solution:
         mp_p = defaultdict(str)
         mp_s = defaultdict(str)
         for i in range(len(pattern)):
-            if pattern[i] not in mp_p:
-                mp_p[pattern[i]] = s[i]
-            elif pattern[i] in mp_p and mp_p[pattern[i]] != s[i]:
+            if pattern[i] in mp_p and mp_p[pattern[i]] != s[i]:
                 return False
-            if s[i] not in mp_s:
-                mp_s[s[i]] = pattern[i]
-            elif s[i] in mp_s and mp_s[s[i]] != pattern[i]:
+            mp_p[pattern[i]] = s[i]
+            if s[i] in mp_s and mp_s[s[i]] != pattern[i]:
                 return False
+            mp_s[s[i]] = pattern[i]
         return True
