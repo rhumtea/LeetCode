77d60aa2c819ad85f57e30e80732af206727ab19
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-06-10 11:31:35 -0700

    Time: 0 ms (100%), Space: 17.8 MB (68.64%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,19 +1,25 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         pq = []
+        if a > 0:
+            heappush(pq, (-a, 'a'))
+        if b > 0:
+            heappush(pq, (-b, 'b'))
+        if c > 0:
+            heappush(pq, (-c, 'c'))
         res = ""
-        if a > 0: heappush(pq, [-a, 'a'])
-        if b > 0: heappush(pq, [-b, 'b'])
-        if c > 0: heappush(pq, [-c, 'c'])
         while pq:
-            x1, y1 = heappop(pq)
-            if len(res) >= 2 and res[-1] == res[-2] == y1:
-                if not pq: break
-                x2, y2 = heappop(pq)
-                res += y2
-                if x2+1 < 0: heappush(pq, [x2+1, y2])
-                heappush(pq, [x1,y1])
+            temp = heappop(pq)
+            if len(res) >= 2 and res[-1] == res[-2] == temp[1]:
+                if not pq:
+                    break
+                temp1 = heappop(pq)
+                res += temp1[1]
+                if temp1[0]+1 < 0:
+                    heappush(pq, (temp1[0]+1, temp1[1]))
+                heappush(pq, (temp[0], temp[1]))
             else:
-                res += y1
-                if x1+1 < 0: heappush(pq, [x1+1,y1])
+                res += temp[1]
+                if temp[0]+1 < 0:
+                    heappush(pq, (temp[0]+1, temp[1]))
         return res
