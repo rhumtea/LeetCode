35dcf5c2ac91c038ffe53e77d2d851a245ce661d
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-06-10 17:48:55 -0700

    Time: 44 ms (85.6%), Space: 28.4 MB (98.79%) - LeetHub

--- 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.py
@@ -5,12 +5,10 @@ class Solution:
         for i in range(n-1):
             diff = heights[i+1] - heights[i]
             if diff <= 0: continue
+            heappush(pq, diff)
             if ladders: 
                 ladders -= 1
-                heappush(pq, diff)
             else:
-                heappush(pq, diff)
                 bricks -= heappop(pq)
                 if bricks < 0: return i
         return n-1
-        
