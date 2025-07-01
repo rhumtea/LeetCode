07ea74e1a940169c13a2c718351acded5350721b
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-07-01 15:56:53 -0700

    Time: 3 ms (16.23%), Space: 18.4 MB (14.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,17 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        visited = set()
-        pq = [(sr, sc)]
-        c = image[sr][sc]
         M, N = len(image), len(image[0])
-        while len(pq) >= 1:
-            i, j = heappop(pq)
-            image[i][j] = color
-            if (i, j) not in visited:
-                visited.add((i,j))
-                for dx, dy in dir:
-                    ni, nj = i + dx, j + dy
-                    if 0 <= ni < M and 0 <= nj < N and image[ni][nj] == c:
-                        heappush(pq, (ni,nj))
+        visit = [[False] * N for _ in range(M)]
+        check = image[sr][sc]
+        def dfs(i, j):
+            visit[i][j] = True
+            if image[i][j] == check:
+                image[i][j] = color
+            else:
+                return
+            for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                if  0 <= ni < M and 0 <= nj < N and visit[ni][nj] == False:
+                    dfs(ni, nj)
+        dfs(sr, sc)
         return image
