fca326b6131e16d71110ba397a58124f3a207c7d
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-07-21 23:53:44 -0700

    Time: 93 ms (68.38%), Space: 27 MB (83.74%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,22 +1,9 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = []
-        temperatures.append(float('inf'))
-        st = []
-        max_arr = [[0]*2 for _ in range(len(temperatures))]
-        for i in range(len(temperatures)):
-            while st and temperatures[st[-1]] < temperatures[i]:
-                j = st.pop()
-                L = 0
-                if st:
-                    L = st[-1]+1
-                R = i - 1
-                max_arr[j] = [L, R]
-            st.append(i)
-        for i in range(len(max_arr)-1):
-            if max_arr[i][1] == len(temperatures)-2:
-                res.append(0)
-            else:
-                res.append(max_arr[i][1]+1-i)
-        return res
-
+        stack, result = [], [0]*len(temperatures)
+        for index, temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+                result[stack[-1]] = index - stack[-1]
+                stack.pop()
+            stack.append(index)
+        return result  
