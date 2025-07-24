4aa81388c28c4220b0cf1a7d6eec18531c546f5f
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-07-23 23:48:57 -0700

    Time: 256 ms (44.41%), Space: 20.2 MB (68.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,14 +3,19 @@ class Solution:
         M, N = len(grid), len(grid[0])
         count = 0
         visit = [[False] * N for _ in range(M)]
-        def dfs(i, j):
+        def bfs(i, j):
+            q = deque()
             visit[i][j] = True
-            for ni, nj in [[i+1,j], [i-1,j], [i, j+1], [i, j-1]]:
-                if 0 <= ni < M and 0 <= nj < N and visit[ni][nj] == False and grid[i][j] == "1":
-                    dfs(ni, nj)
+            q.append([i, j])
+            while q:
+                a, b = q.popleft()
+                for na, nb in [[a+1, b], [a-1, b], [a, b+1], [a, b-1]]:
+                    if 0 <= na < M and 0 <= nb < N and visit[na][nb] == False and grid[na][nb] == "1":
+                        q.append([na, nb])
+                        visit[na][nb] = True
         for i in range(M):
-            for j in range(N): 
+            for j in range(N):
                 if visit[i][j] == False and grid[i][j] == "1":
                     count += 1
-                    dfs(i, j)
+                    bfs(i, j)
         return count
