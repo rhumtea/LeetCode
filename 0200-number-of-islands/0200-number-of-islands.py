1b776720a319b1bd96f700d9ad23cf60f6858a7d
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-07-23 23:48:06 -0700

    Time: 301 ms (13.75%), Space: 20.5 MB (37.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,19 +3,14 @@ class Solution:
         M, N = len(grid), len(grid[0])
         count = 0
         visit = [[False] * N for _ in range(M)]
-        def bfs(si, sj):
-            q = deque()
-            visit[si][sj] = True
-            q.append([si, sj])
-            while q:
-                i, j = q.popleft()
-                for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
-                    if 0 <= ni < M and 0 <= nj < N and visit[ni][nj] == False and grid[ni][nj] == "1":
-                        q.append([ni, nj])
-                        visit[ni][nj] = True
-        for si in range(M):
-            for sj in range(N):
-                if visit[si][sj] == False and grid[si][sj] == "1":
+        def dfs(i, j):
+            visit[i][j] = True
+            for ni, nj in [[i+1,j], [i-1,j], [i, j+1], [i, j-1]]:
+                if 0 <= ni < M and 0 <= nj < N and visit[ni][nj] == False and grid[i][j] == "1":
+                    dfs(ni, nj)
+        for i in range(M):
+            for j in range(N): 
+                if visit[i][j] == False and grid[i][j] == "1":
                     count += 1
-                    bfs(si, sj)
+                    dfs(i, j)
         return count
