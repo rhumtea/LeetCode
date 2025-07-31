fdbfd5e53fad2b7aaa8abc2940bf4054b2a2aa98
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-07-30 20:05:01 -0700

    Time: 292 ms (47.77%), Space: 45.1 MB (81.25%) - LeetHub

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        adj = [[] for _ in range(n)]
+        for i in range(n):
+            if manager[i] == -1:
+                continue
+            adj[manager[i]].append(i)
+        res = 0
+        def dfs(cur, p):
+            time = 0
+            for ne in adj[cur]:
+                t = dfs(ne, cur)
+                time = max(time, t)
+            return time + informTime[cur]
+        return dfs(headID, -1)
+        
