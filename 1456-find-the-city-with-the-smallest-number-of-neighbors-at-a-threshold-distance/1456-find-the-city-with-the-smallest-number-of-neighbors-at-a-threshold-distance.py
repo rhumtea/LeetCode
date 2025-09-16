63473e4b69b4ad102c1e59f49227a0571b32d2f5
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-09-15 19:22:17 -0700

    Time: 383 ms (41.76%), Space: 18.9 MB (81.33%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,27 +1,20 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        # use Dijkstra -> O(n^3 * log n)
+        # use Floyd-Washall -> O(n^3)
+        dist = [[inf] * n for _ in range(n)]
+        for a, b, w in edges:
+            dist[a][a] = dist[b][b] = 0
+            dist[a][b] = w
+            dist[b][a] = w
+        for k in range(n):
+            for a in range(n):
+                for b in range(n):
+                    dist[a][b] = min(dist[a][b], dist[a][k] + dist[k][b])
         res = [0] * n
-        adj = defaultdict(list)
-        for u, v, w in edges:
-            adj[u].append([v, w])
-            adj[v].append([u, w])
-        def dijkstra(u):
-            pq = [[0, u]]
-            dist = [inf] * n
-            dist[u] = 0
-            while pq:
-                d, cur = heappop(pq)
-                if dist[cur] < d: continue
-                for ne, w in adj[cur]:
-                    if d + w < dist[ne]:
-                        dist[ne] = d + w
-                        heappush(pq, [d+w, ne])
-            for i in range(n):
-                if i != u and dist[i] <= distanceThreshold:
-                    res[i] += 1
         for i in range(n):
-            dijkstra(i)
+            for j in range(n):
+                if dist[i][j] <= distanceThreshold and i != j:
+                    res[i] += 1
         ans = 0
         for i in range(n):
             if res[i] <= res[ans]:
