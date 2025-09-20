6b106ebdf4c38c4b0c29100aee3b57c15902bf66
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-09-19 18:28:37 -0700

    Time: 266 ms (33.34%), Space: 28 MB (13.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         M, N = len(grid), len(grid[0])
+        p = list(range(M*N))
+        size = [1] * (M*N)
+        R = [0] * (M*N)
+        def find(i, j):
+            d = i * N + j
+            while d != p[d]:
+                d = p[d]
+            return d
+        def union(ui, uj, vi, vj):
+            du, dv = find(ui, uj), find(vi, vj)
+            if du == dv:
+                return False
+            if R[du] < R[dv]:
+                du, dv = dv, du
+            p[dv] = du
+            size[du] += size[dv]
+            if R[du] == R[dv]:
+                R[du] += 1
+            return True
         count = 0
-        visit = [[False] * N for _ in range(M)]
-        def bfs(s_i, s_j):
-            q = deque()
-            visit[s_i][s_j] = True
-            q.append([s_i, s_j])
-            while q:
-                i, j = q.popleft()
-                for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
-                    if 0 <= ni < M and 0 <= nj < N and visit[ni][nj] == False and grid[ni][nj] == "1":
-                        q.append([ni, nj])
-                        visit[ni][nj] = True
-        for s_i in range(M):
-            for s_j in range(N):
-                if visit[s_i][s_j] == False and grid[s_i][s_j] == "1":
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == '0':
                     count += 1
-                    bfs(s_i, s_j)
-        return count
+                    continue
+                for ni, nj in [[i-1, j], [i, j-1]]:
+                    if 0 <= ni < M and 0 <= nj < N and grid[i][j] == grid[ni][nj]:
+                        union(i, j, ni, nj)
+        res = set()
+        for i in range(M):
+            for j in range(N):
+                res.add(find(i, j))
+        return len(res) - count
