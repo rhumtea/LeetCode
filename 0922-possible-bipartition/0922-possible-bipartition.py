2e3766d382fb86c41c3f3ba538c6e5701a00fff7
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2025-10-01 18:44:01 -0700

    Time: 127 ms (5.03%), Space: 22 MB (61.21%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.py
+++ 0922-possible-bipartition/0922-possible-bipartition.py
@@ -1,26 +1,32 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        p = list(range(n+1))
+        R = [0] * (n+1)
+        size = [1] * (n+1)
+        def find(u):
+            while u != p[u]:
+                u = p[u]
+            return u
+        def union (u, v):
+            u, v = find(u), find(v)
+            if u == v:
+                return False
+            if R[u] < R[v]:
+                u, v = v, u
+            p[v] = u
+            size[u] += size[v]
+            if R[u] == R[v]:
+                R[u] += 1
+            return True
         adj = defaultdict(list)
         for a, b in dislikes:
-            adj[b].append(a)
             adj[a].append(b)
-        visit = [0] * (n+1)
-        visit[0] = 1
-        def dfs(cur, p):
-            if visit[cur] == 0:
-                visit[cur] = 3 - visit[p]
-            else:    
-                if visit[cur] == visit[p]:
-                    return False 
-                else:
-                    return True
-            for ne in adj[cur]:
-                if ne != p:
-                    if not dfs(ne, cur):
-                        return False
-            return True
-        for i in range(1, n+1):
-            if visit[i] == 0:
-                if dfs(i, 0) == False:
-                    return False
+            adj[b].append(a)
+            if adj[b]:
+                union(a, adj[b][0])
+            if adj[a]:
+                union(b, adj[a][0])
+        for a, b in dislikes:
+            if find(a) == find(b):
+                return False
         return True
