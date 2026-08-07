beae340914b029c12f64f1428cfc6403ace07def
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-06 23:15:32 -0700

    Time: 2 ms (70.91%), Space: 19.3 MB (28.74%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -8,6 +8,8 @@ class Solution:
         for v in index.values():
             if len(v) >= 3:
                 for i in range(len(v)-2):
-                    ans = abs(v[i]-v[i+1]) + abs(v[i]-v[i+2]) + abs(v[i+1]-v[i+2])
+                    #ans = v[i+1] - v[i] + v[i+2] - v[i] + v[i+2] - v[i+1]
+                    #ans = 2 * (v[i+2] - v[i])
+                    ans = 2 * (v[i+2] - v[i])
                     res = min(res, ans)
         return res if res != inf else -1
