1fb93ec841ebdd4f5cc8409cd2cacf0a67340da8
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-09 21:03:40 -0700

    Time: 4 ms (7.46%), Space: 19.4 MB (25.76%) - LeetHub

--- 3697-compute-decimal-representation/3697-compute-decimal-representation.py
+++ 3697-compute-decimal-representation/3697-compute-decimal-representation.py
@@ -1,11 +1,10 @@
 class Solution:
     def decimalRepresentation(self, n: int) -> List[int]:
         res = []
-        if n % 10 != 0: res.append(n%10)
-        n //= 10
         t = 1
         while n > 0:
-            t *= 10
-            if n%10 != 0: res.append(n%10 * t)
+            digit = n%10
+            if digit != 0: res.append(digit * t)
             n //= 10
+            t *= 10
         return res[::-1]
