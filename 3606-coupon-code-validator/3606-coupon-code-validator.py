55dee5c4110a326cb063b01d4ef90ec9cb5f9689
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-15 11:02:26 -0700

    Time: 11 ms (53.73%), Space: 19.4 MB (93.53%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -1,14 +1,12 @@
 class Solution:
     def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
-        mp = defaultdict(list)
-        b = {"electronics", "grocery", "pharmacy", "restaurant"}
         ALLOWED = set(string.ascii_letters + string.digits + '_')
+        VALID_B = {"electronics", "grocery", "pharmacy", "restaurant"}
+        valid = []
         for i in range(len(code)):
-            is_valid = bool(code[i]) and set(code[i]) <= ALLOWED
-            if businessLine[i] in b and isActive[i] and is_valid:
-                mp[businessLine[i]].append(code[i])
-        res = []
-        for k,v in sorted(mp.items()):
-            for code in sorted(v):
-                res.append(code)
-        return res
+            c = code[i]
+            b = businessLine[i]
+            if isActive[i] and b in VALID_B and c and set(c) <= ALLOWED:
+                valid.append((b,c))
+        valid.sort()
+        return [c for b,c in valid]
