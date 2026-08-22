d56a11891274e7cfcc7f62b04b7ff63c7dcc8f5f
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-22 11:04:08 -0700

    Time: 63 ms (6.91%), Space: 32.7 MB (9.04%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -4,4 +4,10 @@ class Solution:
         pref = [0] * len(nums)
         for i in range(1, len(nums)):
             pref[i] = nums[i] + pref[i-1]
-        return max(pref) - min(pref)
+        res = minn = maxx = 0
+        for r in range(1, len(pref)):
+            t = pref[r]
+            res = max(res, t - minn, maxx - t)
+            minn = min(minn, t)
+            maxx = max(maxx, t)
+        return res
