3623714b2cfccbb1b7b27f1dad45c9a298108744
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-24 15:12:07 -0700

    Time: 115 ms (6.69%), Space: 26.3 MB (5.09%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,20 +1,15 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        def prefix_diff(nums):
-            prefix_diff = [0] * len(nums)
-            num0 = num1 = 0
-            prefix_diff[0] = -1 if nums[0] == 0 else 1
-            for i in range(1, len(nums)):
-                temp = -1 if nums[i] == 0 else 1
-                prefix_diff[i] = prefix_diff[i-1] + temp
-            return prefix_diff
-        res = 0
-        prefix_diff = prefix_diff(nums)
+        nums = [0] + nums
+        pref = [0] * len(nums)
+        for i in range(1, len(nums)):
+            pref[i] = nums[i] + pref[i-1]
+
         mp = defaultdict(int)
-        mp[0] = -1
-        for r in range(len(prefix_diff)):
-            if prefix_diff[r] in mp:
-                res = max(res, r - mp[prefix_diff[r]])
-            else:
-                mp[prefix_diff[r]] = r
+        mp[0] = 0
+        res = 0
+        for r in range(1, len(pref)):
+            t = r - 2 * pref[r]
+            if t in mp: res = max(res, r - mp[t])
+            if t not in mp: mp[t] = r
         return res
