977176c5e2083f5a643d2ab47323c8433d0d854c
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-28 23:26:46 -0700

    Time: 11 ms (61.3%), Space: 19.2 MB (93.89%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
@@ -1,20 +1,15 @@
 class Solution:
     def maxDigitRange(self, nums: list[int]) -> int:
         def digit_range(num):
-            smallest = 10
-            largest = -1
-            while num > 0:
-                d = num%10
-                smallest = min(smallest, d)
-                largest =  max(largest, d)
-                num //= 10
-            return largest - smallest
-
+            digits =  [int(ch) for ch in str(abs(num))]
+            return max(digits) - min(digits)
+        maxx = -1
         res = 0
-        mp = defaultdict(list)
-        max_digit = 0
         for num in nums:
             t = digit_range(num)
-            mp[t].append(num)
-            max_digit = max(max_digit, t)
-        return sum(mp[max_digit])
+            if  t > maxx:
+                maxx = t
+                res =  num
+            elif t == maxx:
+                res += num
+        return res
