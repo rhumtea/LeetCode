8ce6d4b7c67e3ecea12204f77348a6757fc91a1d
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-29 11:23:18 -0700

    Time: 148 ms (30.45%), Space: 25 MB (39.71%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,12 +1,15 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         def max_k(x):
-            res = l = w = 0
-            for r in range(len(nums)):
+            res = w = 0
+            l = 1
+            for r in range(1, len(nums)):
                 w += nums[r]%2
                 while w > x:
                     w -= nums[l]%2
                     l += 1
                 res += (r-l+1)
             return res
+        n = len(nums)
+        nums = [0] + nums
         return max_k(k) - max_k(k-1)
