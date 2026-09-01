7ee2c01aa0d855c1e0cf4716dcf630cb60f090dd
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-08-31 23:59:36 -0700

    Time: 7 ms (9.86%), Space: 19.3 MB (42.97%) - LeetHub

--- 4024-nearest-available-drone/4024-nearest-available-drone.py
+++ 4024-nearest-available-drone/4024-nearest-available-drone.py
@@ -1,10 +1,10 @@
 class Solution:
     def nearestDrone(self, drones: list[list[int]], target: list[int]) -> int:
         res = -1
-        minn = inf
+        smallest = inf
         for i in range(len(drones)):
             t = abs(drones[i][0] - target[0]) + abs(drones[i][1] - target[1])
-            if t <= drones[i][2] and t < minn:
-                minn = t
+            if t <= drones[i][2] and t < smallest:
+                smallest = t
                 res = i
         return res
