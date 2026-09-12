2a86b6c1ef753fd183c16023f11f4ea180ff43ec
Phong Truong <160001872+rhumtea@users.noreply.github.com>
2026-09-11 23:54:25 -0700

    Time: 148 ms (23.78%), Space: 19.6 MB (86.14%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,6 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        max_freq = 0
+        maxx = 0
         w = defaultdict(int)
         l = 0
         for r in range(len(s)):
@@ -8,5 +8,5 @@ class Solution:
             while (r-l+1) - max(w.values()) > k:
                 w[s[l]] -= 1
                 l += 1
-            max_freq = max(max_freq, r-l+1)
-        return max_freq
+            maxx = max(maxx, r-l+1)
+        return maxx
